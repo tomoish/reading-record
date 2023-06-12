d2047ee4aee613abaf81388a841e524e71e2f6ad
tomoish <tomo.i.120608@gmail.com>
2023-06-13 00:35:11 +0900

    write README.md and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # reading_record
+
+reading_record is a web application that allows you to record your reading content.
+
+## Web
+https://django-reading-record.com
+
+## Technologies Used
+- Django 
+- PostgresQL
+- Nginx
+
+## Production Environment
+- AWS EC2
+
+
--- reading_record/urls.py
+++ reading_record/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('register/', views.AccountRegistration.as_view(), name='register'),
-    # path('', views.index, name='index'),
-    # path('login/', views.LoginView.as_view(), name='login'),
     path('login/',views.Login,name='Login'),
     path('',views.Logout,name='Logout'),
     path('home/',views.home,name='home'),
